3a9e3c254a070a5f20f660a6e3b5623d20006815
Christopher Sang <csdev@users.noreply.github.com>
2023-02-17 16:31:39 -0500

    feat: parse commit body and footers

--- internal/commit/commit.go
+++ internal/commit/commit.go
@@ -10,6 +10,55 @@ import (
 	git "github.com/libgit2/git2go/v34"
 )
 
+type Footer struct {
+	Token string
+	Value string
+}
+
+// TODO: handle "BREAKING CHANGE" properly (the only footer that allows whitespace,
+// is case-sensitive, and requires the separator to be ": ")
+var footerPattern = regexp.MustCompile(`^` +
+	`(?P<token>[^:\pZ\x09-\x0D\x{FEFF}]+)` +
+	`(?P<separator>: | #)` +
+	`(?P<value>.*)` +
+	`$`)
+
+func extractFooters(lines []string) []Footer {
+	footers := make([]Footer, 0, 5)
+	var token string
+	var value strings.Builder
+
+	for _, line := range lines {
+		match := footerPattern.FindStringSubmatch(line)
+		if match == nil {
+			if token == "" {
+				// first line is not a footer -- abort
+				// this allows us to distinguish a footers section
+				// from the last paragraph of the body
+				return []Footer{}
+			} else {
+				// continuation of previous footer
+				// (conventional commits allows footer values to span multiple lines)
+				value.WriteString("\n")
+				value.WriteString(line)
+			}
+		} else {
+			if token != "" {
+				footers = append(footers, Footer{token, value.String()})
+			}
+			token = match[footerPattern.SubexpIndex("token")]
+			value.Reset()
+			value.WriteString(match[footerPattern.SubexpIndex("value")])
+		}
+	}
+
+	if token != "" {
+		footers = append(footers, Footer{token, value.String()})
+	}
+
+	return footers
+}
+
 // Commit represents a single conventional commit.
 type Commit struct {
 	Id          string
@@ -18,7 +67,7 @@ type Commit struct {
 	IsExclaimed bool
 	Description string
 	Body        string
-	Footers     [][2]string // list of (token, value), preserving order and duplicates
+	Footers     []Footer
 }
 
 // based on https://github.com/conventional-commits/parser/tree/v0.4.1#the-grammar
@@ -54,7 +103,56 @@ func (c *Commit) setMessage(msg string) error {
 	if ok := scanner.Scan(); !ok {
 		return fmt.Errorf("%s: syntax error: message cannot be empty", c.Id)
 	}
-	c.setFirstLine(scanner.Text())
+	err := c.setFirstLine(scanner.Text())
+	if err != nil {
+		return err
+	}
+
+	if ok := scanner.Scan(); !ok {
+		return nil // end of commit message (no body or footers)
+	}
+
+	if scanner.Text() != "" {
+		return fmt.Errorf("%s: syntax error: the commit summary must be followed by a blank line", c.Id)
+	}
+
+	// The body of the commit message may consist of multiple paragraphs,
+	// each separated by a blank line. The final paragraph may be part of
+	// the body, or it may actually be the footers.
+	// Read in the remainder of the message, and keep track of where the final
+	// paragraph begins, so we can apply footer matching to it.
+
+	lines := make([]string, 0, 10)
+	lineNum := 0
+	parStart := -1
+	isPar := false
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			isPar = false
+		} else if !isPar {
+			isPar = true
+			parStart = lineNum
+		}
+
+		lines = append(lines, line)
+		lineNum += 1
+	}
+
+	if parStart >= 0 {
+		footers := extractFooters(lines[parStart:])
+		if len(footers) == 0 {
+			// No footers were detected. The commit body is the entire
+			// block of text.
+			c.Body = strings.Join(lines, "\n")
+		} else {
+			// Footers were extracted from the final paragraph.
+			// The commit body consists of all the previous paragraphs.
+			c.Body = strings.TrimRight(strings.Join(lines[:parStart], "\n"), "\n")
+			c.Footers = footers
+		}
+	}
 
 	return nil
 }
@@ -133,10 +231,9 @@ func (c *Commit) ApplyPolicy(policy config.Policy) error {
 	// For example, Github uses one "Co-authored-by" footer for each co-author.
 	// https://docs.github.com/en/pull-requests/committing-changes-to-your-project/creating-and-editing-commits/creating-a-commit-with-multiple-authors
 	if c.Footers != nil {
-		for _, footer := range c.Footers {
-			token := footer[0]
-			if policy.Footer.Tokens != nil && !policy.Footer.Tokens.Contains(token) {
-				return fmt.Errorf("%s: policy does not allow footer token: %s", c.Id, token)
+		for _, f := range c.Footers {
+			if policy.Footer.Tokens != nil && !policy.Footer.Tokens.Contains(f.Token) {
+				return fmt.Errorf("%s: policy does not allow footer token: %s", c.Id, f.Token)
 			}
 		}
 		// TODO: check requiredTokens
--- internal/commit/commit_test.go
+++ internal/commit/commit_test.go
@@ -7,6 +7,79 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestExtractFooters(t *testing.T) {
+	tests := []struct {
+		description string
+		lines       []string
+		footers     []Footer
+	}{
+		{
+			description: "empty lines has no footers",
+			lines:       []string{},
+			footers:     []Footer{},
+		},
+		{
+			description: "plain text has no footers",
+			lines:       []string{"some body text"},
+			footers:     []Footer{},
+		},
+		{
+			description: "the first line must be a footer",
+			lines: []string{
+				"some body text",
+				"Refs: 1234",
+			},
+			footers: []Footer{},
+		},
+		{
+			description: "footers can have different separators",
+			lines: []string{
+				"Refs #1234",
+				"Co-authored-by: John Doe <john.doe@example>",
+			},
+			footers: []Footer{
+				{"Refs", "1234"},
+				{"Co-authored-by", "John Doe <john.doe@example>"},
+			},
+		},
+		{
+			description: "footers can have duplicate tokens",
+			lines: []string{
+				"Refs: 1234",
+				"Refs: 5678",
+			},
+			footers: []Footer{
+				{"Refs", "1234"},
+				{"Refs", "5678"},
+			},
+		},
+		{
+			description: "footer values can span multiple lines",
+			lines: []string{
+				"Addendum: foo",
+				"bar",
+				"baz",
+			},
+			footers: []Footer{
+				{"Addendum", "foo\nbar\nbaz"},
+			},
+		},
+		{
+			description: "standard footer tokens cannot have whitespace",
+			lines: []string{
+				"issue ref: 1234",
+			},
+			footers: []Footer{},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			assert.Equal(t, test.footers, extractFooters(test.lines))
+		})
+	}
+}
+
 func TestSetFirstLine(t *testing.T) {
 	syntaxErr := errors.New("0: syntax error: message does not have a proper type/scope/description")
 
@@ -140,6 +213,129 @@ func TestSetFirstLine(t *testing.T) {
 		t.Run(test.description, func(t *testing.T) {
 			c := NewCommit("0")
 			err := c.setFirstLine(test.message)
+			assert.Equal(t, test.commit, c)
+			assert.Equal(t, test.err, err)
+		})
+	}
+}
+
+func TestSetMessage(t *testing.T) {
+	tests := []struct {
+		description string
+		message     string
+		commit      *Commit
+		err         error
+	}{
+		{
+			description: "summary only",
+			message:     "feat: implement the thing\n",
+			commit: &Commit{
+				Id:          "0",
+				Type:        "feat",
+				Description: "implement the thing",
+			},
+			err: nil,
+		},
+		{
+			description: "summary with extra blank lines",
+			message:     "feat: implement the thing\n\n",
+			commit: &Commit{
+				Id:          "0",
+				Type:        "feat",
+				Description: "implement the thing",
+			},
+			err: nil,
+		},
+		{
+			description: "summary and body",
+			message:     "feat: implement the thing\n\ndescription line 1\ndescription line 2\n",
+			commit: &Commit{
+				Id:          "0",
+				Type:        "feat",
+				Description: "implement the thing",
+				Body:        "description line 1\ndescription line 2",
+			},
+			err: nil,
+		},
+		{
+			description: "summary and multiple body paragraphs",
+			message:     "feat: implement the thing\n\n1a\n1b\n\n2a\n2b\n",
+			commit: &Commit{
+				Id:          "0",
+				Type:        "feat",
+				Description: "implement the thing",
+				Body:        "1a\n1b\n\n2a\n2b",
+			},
+			err: nil,
+		},
+		{
+			description: "summary and footers",
+			message:     "feat: implement the thing\n\nRefs: #1234\nSigned-off-by: John Doe <john.doe@example>\n",
+			commit: &Commit{
+				Id:          "0",
+				Type:        "feat",
+				Description: "implement the thing",
+				Footers: []Footer{
+					{"Refs", "#1234"},
+					{"Signed-off-by", "John Doe <john.doe@example>"},
+				},
+			},
+		},
+		{
+			description: "summary, body, and footers",
+			message:     "feat: implement the thing\n\n1a\n1b\n\n2a\n2b\n\nRefs: #1234",
+			commit: &Commit{
+				Id:          "0",
+				Type:        "feat",
+				Description: "implement the thing",
+				Body:        "1a\n1b\n\n2a\n2b",
+				Footers: []Footer{
+					{"Refs", "#1234"},
+				},
+			},
+			err: nil,
+		},
+		{
+			description: "multi-line footers",
+			message:     "feat: implement the thing\n\nBREAKING-CHANGE: the api\nis different\n",
+			commit: &Commit{
+				Id:          "0",
+				Type:        "feat",
+				Description: "implement the thing",
+				Footers: []Footer{
+					{"BREAKING-CHANGE", "the api\nis different"},
+				},
+			},
+			err: nil,
+		},
+		{
+			description: "message cannot be empty",
+			message:     "",
+			commit:      &Commit{Id: "0"},
+			err:         errors.New("0: syntax error: message cannot be empty"),
+		},
+		{
+			description: "first line must be correct",
+			message:     "asdf",
+			commit:      &Commit{Id: "0"},
+			err:         errors.New("0: syntax error: message does not have a proper type/scope/description"),
+		},
+		{
+			description: "blank line needed between summary and body",
+			message:     "feat: implement the thing\nasdf\n",
+			commit: &Commit{
+				Id:          "0",
+				Type:        "feat",
+				Description: "implement the thing",
+			},
+			err: errors.New("0: syntax error: the commit summary must be followed by a blank line"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			c := NewCommit("0")
+			err := c.setMessage(test.message)
 			assert.Equal(t, test.commit, c)
 			assert.Equal(t, test.err, err)
 		})
